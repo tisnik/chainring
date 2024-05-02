405026a6cd5be332194ce368034c525e637e774c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-02 08:33:21 +0200

    Use f-strings instead of explicitly calling .format method

--- src/draw_service.py
+++ src/draw_service.py
@@ -24,7 +24,7 @@ class DrawServiceInterface:
     @staticmethod
     def get_url(address, port):
         """Get URL prefix to the service."""
-        return "http://{address}:{port}".format(address=address, port=port)
+        return f"http://{address}:{port}"
 
     def __init__(self, service_url="http://localhost:3000"):
         """Initialize the interface."""
@@ -32,11 +32,7 @@ class DrawServiceInterface:
 
     def get(self, endpoint):
         """Get full URL to selected endpoint."""
-        url = "{url}/{api}/{endpoint}".format(
-            url=self._service_url,
-            api=DrawServiceInterface.API_PREFIX,
-            endpoint=endpoint,
-        )
+        url = f"{self._service_url}/{DrawServiceInterface.API_PREFIX}/{endpoint}"
         response = requests.get(url, timeout=10)
         return response.status_code, response.json()
 
@@ -45,7 +41,7 @@ class DrawServiceInterface:
         try:
             code, data = self.get("liveness")
             if code != 200:
-                return False, "Návratový kód {code}".format(code=code)
+                return False, f"Návratový kód {code}"
             if "status" in data and data["status"] == "ok":
                 return True, None
             return False, "Neplatná data vrácená serverem"
@@ -57,7 +53,7 @@ class DrawServiceInterface:
         try:
             code, data = self.get("info")
             if code != 200:
-                return False, "Návratový kód {code}".format(code=code)
+                return False, f"Návratový kód {code}"
             if "service-version" in data:
                 return True, data["service-version"], "Success"
             return False, None, "Neplatná data vrácená serverem"
@@ -69,7 +65,7 @@ class DrawServiceInterface:
         try:
             code, data = self.get(url)
             if code != 200:
-                return None, "Návratový kód {code}".format(code=code)
+                return None, f"Návratový kód {code}"
             if "status" in data and data["status"] == "ok":
                 if selector in data:
                     return data[selector], "Ok"
@@ -81,28 +77,22 @@ class DrawServiceInterface:
 
     def read_areals(self, valid_from):
         """Read list of areals from the web service."""
-        url = "areals?valid-from={date}".format(date=valid_from)
+        url = f"areals?valid-from={valid_from}"
         return self.read_aoid(url, "areals", "Seznam areálů je prázdný")
 
     def read_buildings(self, valid_from, aoid):
         """Read list of buildings from the web service."""
-        url = "buildings?valid-from={date}&areal-id={aoid}".format(
-            date=valid_from, aoid=aoid
-        )
+        url = f"buildings?valid-from={valid_from}&areal-id={aoid}"
         return self.read_aoid(url, "buildings", "Seznam budov je prázdný")
 
     def read_floors(self, valid_from, aoid):
         """Read list of floors from the web service."""
-        url = "floors?valid-from={date}&building-id={aoid}".format(
-            date=valid_from, aoid=aoid
-        )
+        url = f"floors?valid-from={valid_from}&building-id={aoid}"
         return self.read_aoid(url, "floors", "Seznam podlaží je prázdný")
 
     def read_rooms(self, valid_from, aoid):
         """Read list of rooms from the web service."""
-        url = "rooms?valid-from={date}&floor-id={aoid}".format(
-            date=valid_from, aoid=aoid
-        )
+        url = f"rooms?valid-from={valid_from}&floor-id={aoid}"
         return self.read_aoid(url, "rooms", "Seznam místností je prázdný")
 
     def check_input_data(self, data):
@@ -116,12 +106,8 @@ class DrawServiceInterface:
 
     def send_drawing(self, drawing):
         """Send drawing onto the web service."""
-        endpoint = "drawing-data?drawing={id}&format=json".format(id=drawing.drawing_id)
-        url = "{url}/{api}/{endpoint}".format(
-            url=self._service_url,
-            api=DrawServiceInterface.API_PREFIX,
-            endpoint=endpoint,
-        )
+        endpoint = f"drawing-data?drawing={drawing.drawing_id}&format=json"
+        url = f"{self._service_url}/{DrawServiceInterface.API_PREFIX}/{endpoint}"
         json_exporter = JSONExporter("output.json", drawing)
         payload = json_exporter.as_string()
         try:
@@ -129,11 +115,9 @@ class DrawServiceInterface:
             code, data = self.get("info")
             print(code, data)
             if code != 200:
-                return False, "Návratový kód {code}".format(code=code)
+                return False, f"Návratový kód {code}"
             else:
-                message = "Výkres byl uložen pod ID {id} ({b} bajtů)".format(
-                    id=drawing.drawing_id, b=len(payload)
-                )
+                message = f"Výkres byl uložen pod ID {drawing.drawing_id} ({len(payload)} bajtů)"
                 return True, message
         except Exception as e:
             return False, e
@@ -143,7 +127,7 @@ class DrawServiceInterface:
         try:
             code, data = self.get("all-drawings")
             if code != 200:
-                return False, "Návratový kód {code}".format(code=code)
+                return False, f"Návratový kód {code}"
             if self.check_input_data(data):
                 return True, data, "Success"
             return False, None, "Neplatná data"
